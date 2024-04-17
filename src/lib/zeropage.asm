4c36454db5449954ec6e95fd078297c0fb412ed6
jonknibbs <git@talkingdead.co.uk>
2024-04-17 21:30:23 +0100

    Add zero page parameters

---
+++ src/lib/zeropage.asm
@@ -0,0 +1,16 @@
+ZERO_PAGE: {
+    .label TEMP = $02
+    .label PARAM_1 = $73
+    .label PARAM_2 = $74
+    .label PARAM_3 = $75
+    .label PARAM_4 = $76
+    .label PARAM_5 = $77
+    .label PARAM_6 = $78
+    .label PARAM_7 = $79
+    .label PARAM_8 = $7A
+    .label PARAM_9 = $7B
+    .label ADDRESS_LOW = $FB
+    .label ADDRESS_HIGH = $FC
+    .label ADDRESS_LOW_2 = $FD
+    .label ADDRESS_HIGH_2 = $FE
+}
