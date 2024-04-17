5c3174bf2797d3a5151881b2278580ab15290b96
jonknibbs <git@talkingdead.co.uk>
2024-04-17 20:35:04 +0100

    Update Hello world to cycle border colors

--- src/examples/hello-world/hello.asm
+++ src/examples/hello-world/hello.asm
@@ -1,19 +1,29 @@
+#importonce 
+#import "../../lib/screen.asm"
 
 BasicUpstart2(helloWorld)
 *=$3000 "Hello World"
 
 helloWorld:
+
+    lda #BLACK
+    sta SCREEN.SCREEN_COLOR
+
     lda message
     ldx #0
 
     lda message, x
-loop:
+!loop:
     sta $0400, x
     inx
     lda message, x
     cmp #0
-    bne loop
+    bne !loop-
 
+!loop:
+    inx
+    stx SCREEN.BORDER_COLOR
+    jmp !loop-
 
 message:
     .text "hello world"
---
+++ src/lib/screen.asm
@@ -0,0 +1,4 @@
+SCREEN: {
+    .label	BORDER_COLOR = $D020
+	.label	SCREEN_COLOR = $D021
+}
