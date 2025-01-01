90b666356bc55b3747f32544a7e00117eabfdc70
jonknibbs <git@talkingdead.co.uk>
2025-01-01 18:13:38 +0000

    add color helper lib

---
+++ src/lib/color.asm
@@ -0,0 +1,18 @@
+COLOR: {
+    .label black = $0
+    .label white = $1
+    .label red = $2
+    .label cyan = $3
+    .label purple  = $4
+    .label green = $5
+    .label blue = $6
+    .label yellow = $7
+    .label orange = $8
+    .label brown = $9
+    .label lred = $a
+    .label dgrey = $b
+    .label grey = $c
+    .label lgreen = $d
+    .label lblue = $e
+    .label lgrey = $f
+}
